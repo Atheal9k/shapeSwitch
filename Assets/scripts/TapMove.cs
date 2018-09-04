ea0722027443a41814adce92ad1df13e3e310e4a
Atheal9k <v.huang435@gmail.com>
2018-09-04 11:36:38 +1000

    Initial Commit

---
+++ Assets/scripts/TapMove.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapMove : MonoBehaviour
+{
+
+    Touch touch;
+    private Vector2 targetPos;
+    public float Xincrement;
+
+    void Update()
+    {
+        
+            if (Input.touchCount > 0)
+            {
+            touch = Input.GetTouch(0);
+
+            if (touch.position.x < Screen.width / 2)
+                {
+                    targetPos = new Vector2(transform.position.x + Xincrement, transform.position.y);
+                    transform.position = targetPos;
+                }
+
+            }
+            else if (touch.position.x > Screen.width / 2)
+            {
+                targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y);
+                transform.position = targetPos;
+            }
+        
+
+           
+
+
+    }
+}
---
+++ Assets/scripts/changeSprite.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class changeSprite : MonoBehaviour {
+
+    public string currentTag;
+    private SpriteRenderer sr;
+
+    public Sprite triangle;
+    public Sprite square;
+    public Sprite star;
+
+
+    void Start()
+    {
+        sr = GetComponent<SpriteRenderer>();
+    }
+
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if (col.gameObject.tag == "star" && currentTag == "star")
+        {
+            
+            SetRandomTag();
+        }
+
+        if (col.gameObject.tag == "triangle" && currentTag == "triangle")
+        {
+
+            SetRandomTag();
+        }
+
+        if (col.gameObject.tag == "square" && currentTag == "square")
+        {
+
+            SetRandomTag();
+        }
+
+        else
+        {
+            
+        }
+    }
+
+    void SetRandomTag()
+    {
+        int index = Random.Range(0,3);
+
+        switch (index)
+        {
+            case 0:
+                currentTag = "star";
+                sr.sprite = star;
+                break;
+            case 1:
+                currentTag = "triangle";
+                sr.sprite = triangle;
+                break;
+            case 2:
+                currentTag = "square";
+                sr.sprite = square;
+                break;
+            
+        }
+       
+    }
+}
